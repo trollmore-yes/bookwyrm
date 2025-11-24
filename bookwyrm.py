47c7f9bddf75d67ac19b88c954ee7ad689a5b27e
trollmore <sttrollmore@gmail.com>
2025-11-24 14:32:31 -0800

    added resources

--- bookwyrm.py
+++ bookwyrm.py
@@ -1,16 +1,25 @@
 import discord
 import os
 from dotenv import load_dotenv
+import channel_manager
 
 load_dotenv()
 bot = discord.Bot()
+
+c_manager = ChannelManager()
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
+# @bot.slash_command(guild_ids=[1274792500975894589])
+# async def hello(ctx):
+#     await ctx.respond("Hello!")
+
 @bot.slash_command(guild_ids=[1274792500975894589])
-async def hello(ctx):
-    await ctx.respond("Hello!")
+async def make_thread_in_forum(ctx, channel : discord.ForumChannel):
+    await channel.create_thread(name="test thread", content="this is a test thread")
+
+
 
 bot.run(os.getenv('TOKEN'))
---
+++ channel-manager.py
@@ -0,0 +1,6 @@
+class ChannelManager():
+    def __init__(self):
+        self.discussion_forum = None
+        self.submission_forum = None
+
+    
