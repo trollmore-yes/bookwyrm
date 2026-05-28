4b9ec7ffa60f3860a51efdade4cad6ce13ea67a7
trollmore <sttrollmore@gmail.com>
2026-05-27 23:11:27 -0700

    fixed up schema

--- migrations/0001-initial.sql
+++ migrations/0001-initial.sql
@@ -1,10 +1,7 @@
-CREATE TABLE signups (
-    timestamp INTEGER NOT NULL,
-    guild_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    genres JSONB NOT NULL,
-    preferred_size TEXT NOT NULL CHECK (preferred_size IN ('small', 'medium', 'large')),
-    PRIMARY KEY (timestamp, guild_id, user_id)
+CREATE TABLE guilds (
+    guild_id INTEGER PRIMARY KEY,
+    discussion_channel_id INTEGER NOT NULL,
+    submission_channel_id INTEGER NOT NULL
 ) STRICT;
 
 CREATE TABLE roles (
@@ -12,6 +9,15 @@ CREATE TABLE roles (
     mascot TEXT NOT NULL,
     role INTEGER NOT NULL UNIQUE,
     PRIMARY KEY (guild_id, mascot)
+) STRICT;
+
+CREATE TABLE signups (
+    timestamp INTEGER NOT NULL,
+    guild_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    genres JSONB NOT NULL,
+    preferred_size TEXT NOT NULL CHECK (preferred_size IN ('small', 'medium', 'large')),
+    PRIMARY KEY (timestamp, guild_id, user_id)
 ) STRICT;
 
 CREATE TABLE groups (
